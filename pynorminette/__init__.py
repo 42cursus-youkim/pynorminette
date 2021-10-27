a615533f1ec744d2faaa8525b4cda871fb316b13
youkim <42.4.youkim@gmail.com>
2021-10-27 15:13:53 +0900

    build: :package: [0.0.2] pynorminette

--- pynorminette/__init__.py
+++ pynorminette/__init__.py
@@ -1,3 +1,10 @@
-from tester import test
+from .checker import check
 
-__all__ = ["test"]
+__all__ = ["check"]
+
+try:
+    import black
+    import flake8
+    import mypy
+except Exception as e:
+    print(e)
--- pynorminette/checker.py
+++ pynorminette/checker.py
@@ -6,8 +6,8 @@ from typing import List
 
 from termcolor import colored, cprint
 
-from status import Status
-from tester import test
+from .status import Status
+from .tester import test
 
 TEST_SUITE: 'dict[str, "list[str]"]' = {
     "black": ["--check", "--line-length", "79"],
@@ -20,6 +20,7 @@ def check(args: List[str]):
     if not len(args):
         return cprint("no files specified!", on_color="on_red")
 
+    cprint(f"testing {', '.join(args)}", attrs=["bold"])
     results = [
         test(k, args, flags=v) == Status.OK for k, v in TEST_SUITE.items()
     ]
@@ -32,4 +33,5 @@ def check(args: List[str]):
 
 
 if __name__ == "__main__":
-    check(argv[1:])
+    check(["test_ko.py"])
+    check(["test_ok.py"])

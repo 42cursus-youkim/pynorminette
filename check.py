db9557f29b74fe83b9f8043dacb770a68dddca07
youkim <42.4.youkim@gmail.com>
2021-10-27 13:38:39 +0900

    refactor: :recycle: refactor code

--- check.py
+++ check.py
@@ -5,27 +5,31 @@ from sys import argv
 from pynorminette_package import test, Status
 
 from typing import List
-from termcolor import cprint
+from termcolor import cprint, colored
 
 test_suite: 'dict[str, "list[str]"]' = {
+    "black": ["--check", "--line-length", "79"],
     "mypy": [],
-    "black": ["--check"],
     "flake8": [],
-    # "pylint": ['-q',  "--output-format=colorized"],
 }
 
 
 def main():
     args = argv[1:]
-    results = [
-        test(k, args, flags=v) == Status.OK for k, v in test_suite.items()
-    ]
-    print(Status(not all(results)))
-
-
-if __name__ == "__main__":
-    if len(argv) <= 1:
+    if not len(args):
         cprint("no files specified!", on_color="on_red")
         exit(1)
 
+    results = [
+        test(k, args, flags=v) == Status.OK for k, v in test_suite.items()
+    ]
+    is_pass = Status(not all(results))
+    score = colored(
+        f"[{results.count(True)}/{len(results)}] ",
+        on_color=["on_green", "on_red"][is_pass],
+    )
+    print(score, is_pass, sep="")
+
+
+if __name__ == "__main__":
     main()
